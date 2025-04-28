6457a4494d9c0adc6761a5486878e0fb33ec6803
Luis Eduardo <luiseduardo250305@gmail.com>
2025-04-28 10:48:57 -0300

    Criando a rota get de login

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+//Configurações
 const express = require("express")
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
-
 const PORTA = 3000
+
+//Rotas
+const loginRouter = require("./routes/login")
+app.use("/",loginRouter)
 
 app.listen(PORTA,()=>{
     console.log(`Servidor rodando na porta ${PORTA}`)
